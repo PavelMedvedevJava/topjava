a81d4d5e57a0bac62acd6a3a54d7782676a73e06
PavelMedvedevJava <darksnakes@mail.ru>
2020-02-08 22:24:29 +0500

    1 2 HW0 cycles

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -9,6 +9,8 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static ru.javawebinar.topjava.util.TimeUtil.isBetweenInclusive;
+
 public class MealsUtil {
     public static void main(String[] args) {
         List<Meal> meals = Arrays.asList(
